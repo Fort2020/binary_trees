c7b473a69a4f76d8305518a4468dddee637d6ee9
fort2020 <frankfortdata@gmail.com>
2023-03-30 00:29:16 -0700

    Updated

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -7,13 +7,15 @@
  */
 bst_t *bst_search(const bst_t *tree, int value)
 {
-if (tree == NULL)
-return (NULL);
-if (tree->n == value)
-return ((void *)tree);
-if (tree->n > value)
-return (bst_search(tree->left, value));
-if (tree->n < value)
-return (bst_search(tree->right, value));
-return (NULL);
+
+	if (tree == NULL)
+		return (NULL);
+	if (value == tree->n)
+		return ((bst_t *)tree);
+	if (value < tree->n)
+		return (bst_search(tree->left, value));
+	else
+		return (bst_search(tree->right, value));
+	return (NULL);
 }
+
